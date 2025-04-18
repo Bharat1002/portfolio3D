7ef74b477da8935a967ff8a293c90ee8296c1a5a
Bharat1002 <bharatjc1002@gmail.com>
2025-04-18 20:27:39 +0530

    feat: Navbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
     </>
